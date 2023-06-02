fdd468754814bc2077025deec87ab6de44225264
nielsperdon <niels.perdon@gmail.com>
2023-06-02 13:42:17 +0200

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import Banner from "../components/banner/banner";
+import NavBar from "../components/nav/navbar";
+
 export default function Home() {
  return (
 <div className={styles.container}>
@@ -8,15 +10,15 @@ export default function Home() {
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <NavBar username = "Niels"/>
       <Banner
         title="Clifford the red dog"
         subTitle="a very cute dog"
         imgUrl="/static/clifford2.jpg"
       />
 
-      {/* <Navbar />
-      <Card /> */}
+     
+      {/* <Card /> */}
     </div>
  );
 }
