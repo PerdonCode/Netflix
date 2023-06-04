359224d5326f3f052a09beccabccda51eb137a8f
nielsperdon <niels.perdon@gmail.com>
2023-06-04 16:53:33 +0200

    add youtube popular api and refactor code

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Head from "next/head";
 import Banner from "../components/banner/banner";
 import Card from "../components/card/card";
 import SectionCards from "../components/card/section-cards";
-import getVideos from "../lib/videos"
+// these needs to be exported as default but you can only export 1 function on a page but you can import as default
+import { default as getVideos, getPopularVideos} from "../lib/videos"
 
 
 // Side has to start with capital S
@@ -12,6 +13,7 @@ export async function getServerSideProps(){
     const disneyVideos = await getVideos("disney trailer");
     const productivityVideos = await getVideos("productivity");
     const travelVideos = await getVideos("travel");
+    const popularVideos = await getPopularVideos();
     //const popularVideos = await getVideos("disney trailer");
 
   return {
@@ -19,16 +21,20 @@ export async function getServerSideProps(){
       disneyVideos, // will be passed at the page component as props
       travelVideos,
       productivityVideos,
+      popularVideos
     },
   }
 }
-export default function Home({disneyVideos, travelVideos, productivityVideos}) {
+export default function Home({disneyVideos, travelVideos, productivityVideos, popularVideos}) {
  return (
 <div className={styles.container}>
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="styles.main">
+
+     
       <Banner
         title="Clifford the red dog"
         subTitle="a very cute dog"
@@ -37,10 +43,11 @@ export default function Home({disneyVideos, travelVideos, productivityVideos}) {
       <div className={styles.sectionWrapper}>
         {/* array of objects */}
         <SectionCards title="Disney" videos={disneyVideos} size="large" />
-        <SectionCards title="Productivity" videos={travelVideos} size="medium" />
-        <SectionCards title="Popular" videos={productivityVideos} size="small" />
+        <SectionCards title="travel" videos={travelVideos} size="medium" />
+        <SectionCards title="Productivity" videos={productivityVideos} size="small" />
+        <SectionCards title="Popular" videos={popularVideos} size="small" />
       </div>
-      
+       </div>
     </div>
  );
 }
