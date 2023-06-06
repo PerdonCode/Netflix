ac4444c52fae84fff9d5826d732a7feadfe81912
nielsperdon <niels.perdon@gmail.com>
2023-06-06 14:02:43 +0200

    VideoPage api fix

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,77 @@
-import styles from "../styles/Home.module.css";
 import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/banner";
-import Card from "../components/card/card";
-import SectionCards from "../components/card/section-cards";
 import NavBar from "../components/nav/navbar";
-// these needs to be exported as default but you can only export 1 function on a page but you can import as default
-import { default as getVideos, getPopularVideos} from "../lib/videos"
+import SectionCards from "../components/card/section-cards";
 
+import {
+  getVideos,
+  getPopularVideos,
+} from "../lib/videos";
 
-// Side has to start with capital S
-export async function getServerSideProps(){
+export async function getServerSideProps(context) {
 
-    const disneyVideos = await getVideos("disney trailer");
-    const productivityVideos = await getVideos("productivity");
-    const travelVideos = await getVideos("travel");
-    const popularVideos = await getPopularVideos();
-    //const popularVideos = await getVideos("disney trailer");
+  // if (!userId) {
+  //   return {
+  //     props: {},
+  //     redirect: {
+  //       destination: "/login",
+  //       permanent: false,
+  //     },
+  //   };
+  // }
 
+  const disneyVideos = await getVideos("disney trailer");
+  const productivityVideos = await getVideos("Productivity");
+
+  const travelVideos = await getVideos("indie music");
+
+  const popularVideos = await getPopularVideos();
   return {
     props: {
-      disneyVideos, // will be passed at the page component as props
+      disneyVideos,
       travelVideos,
       productivityVideos,
-      popularVideos
+      popularVideos,
+  
     },
-  }
+  };
 }
-export default function Home({disneyVideos, travelVideos, productivityVideos, popularVideos}) {
- return (
-<div className={styles.container}>
+
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+  popularVideos,
+}) {
+  return (
+    <div className={styles.container}>
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="styles.main">
-        
-      <Banner
-        videoId="4zH5iYM4wJo"
-        title="Clifford the red dog"
-        subTitle="a very cute dog"
-        imgUrl="/static/clifford2.jpg" />
 
-      <div className={styles.sectionWrapper}>
-        {/* array of objects */}
-        <SectionCards title="Disney" videos={disneyVideos} size="large" />
-        <SectionCards title="travel" videos={travelVideos} size="medium" />
-        <SectionCards title="Productivity" videos={productivityVideos} size="small" />
-        <SectionCards title="Popular" videos={popularVideos} size="small" />
+      <div className={styles.main}>
+        <NavBar />
+        <Banner
+          videoId="4zH5iYM4wJo"
+          title="Clifford the red dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/clifford2.jpg"
+        />
+
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Disney" videos={disneyVideos} size="large" />
+          
+          <SectionCards title="Travel" videos={travelVideos} size="small" />
+          <SectionCards
+            title="Productivity"
+            videos={productivityVideos}
+            size="medium"
+          />
+          <SectionCards title="Popular" videos={popularVideos} size="small" />
+        </div>
       </div>
-       </div>
     </div>
- );
+  );
 }
